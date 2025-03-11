d2f411b3d7746e7ba629a986160d3fb256e07aa7
OctopusSW <hugo.dlbcq@gmail.com>
2025-03-11 16:07:10 +0100

    all good all fine. this is the end of the pheonix

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 16:20:39 by hdelbecq          #+#    #+#             */
-/*   Updated: 2025/03/11 15:59:31 by hdelbecq         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:06:58 by hdelbecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,7 @@ void	*superpower(void *arg)
 		if ((tmp->t_die && get_ms() >= tmp->t_die) && !data->is_dead)
 		{
 			data->is_dead = 1;
-			printf("%ld %i die\n", get_ms() - data->t_reference,
-				data->philo->id);
+			printf("%ld %i die\n", get_ms() - data->t_reference, tmp->id);
 		}
 		if (data->is_dead)
 			break ;
